4039c8272352a148917ed9c5ee3ba674afdcee0b
Nick Dilßner <nick.dilssner@kirchbergerknorr.de>
2018-01-12 12:47:03 +0100

    Code Version 1.0.0 - review changes  + (renamed) class and scope  + (changed) '<COPY' comments to KK A-Team standard

--- app/code/community/Kirchbergerknorr/UrlCacheControl/Model/Design/Package.php
+++ app/code/community/Kirchbergerknorr/UrlCacheControl/Model/Design/Package.php
@@ -4,7 +4,7 @@
  * @copyright   Copyright (c) 2018 kirchbergerknorr GmbH (http://www.kirchbergerknorr.de)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Kirchbergerknorr_UrlCacheControl_Mage_Core_Model_Design_Package extends Mage_Core_Model_Design_Package
+class Kirchbergerknorr_UrlCacheControl_Model_Design_Package extends Mage_Core_Model_Design_Package
 {
     /**
      * parameter _type
@@ -18,20 +18,21 @@ class Kirchbergerknorr_UrlCacheControl_Mage_Core_Model_Design_Package extends Ma
      */
     public function getSkinUrl($file = null, array $params = array())
     {
-        // <COPY - optimized with getFilename() (contains updateParamDefaults(), but without referenced params - that means all changes happens in parent, except 'type')
-        //         and excluded $params['_default'], because updateParamDefaults() also handles that
         if (empty($params['_type'])) {
-            // not sure if this is really the best, because that means if i would set a type, this method also get other url's the skin
             $params['_type'] = self::SKIN;
         }
 
+        // <kk: - optimized with getFilename() (contains updateParamDefaults(), but without referenced params - that means all changes happens in parent, except 'type')
+        //         and excluded $params['_default'], because updateParamDefaults() also handles that
         $filename = $this->getFilename($file, $params);
-        // COPY>
+        // kk>
 
+        // <kk: - after copied collecting of real file-path is done and after parent processing, add last-modified timestamp as url parameter
         return $this->addModifiedParam(
             parent::getSkinUrl($file, $params),
             $filename
         );
+        // kk>
     }
 
     /**
@@ -41,9 +42,10 @@ class Kirchbergerknorr_UrlCacheControl_Mage_Core_Model_Design_Package extends Ma
      */
     public function getMergedCssUrl($files)
     {
-        // <COPY - really complete different to JS? why is css more secure or complicated then js?
         $isSecure = Mage::app()->getRequest()->isSecure();
+        // <kk: - really complete different to JS? why is css more secure or complicated then js?
         $mergerDir = $isSecure ? 'css_secure' : 'css';
+        // kk>
         $targetDir = $this->_initMergerDir($mergerDir);
         if (!$targetDir) {
             return '';
@@ -59,13 +61,15 @@ class Kirchbergerknorr_UrlCacheControl_Mage_Core_Model_Design_Package extends Ma
 
         // merge into target file
         $targetFilename = md5(implode(',', $files) . "|{$hostname}|{$port}") . '.css';
-        // COPY>
 
+        // <kk: - after copied collecting of real file-path is done and after parent processing, add last-modified timestamp as url parameter
+        //        all the other stuff will handle parent method
         $filename = $targetDir . DS . $targetFilename;
         return $this->addModifiedParam(
             parent::getMergedCssUrl($files),
             $filename
         );
+        // kk>
     }
 
     /**
@@ -75,19 +79,19 @@ class Kirchbergerknorr_UrlCacheControl_Mage_Core_Model_Design_Package extends Ma
      */
     public function getMergedJsUrl($files)
     {
-        // <COPY
         $targetFilename = md5(implode(',', $files)) . '.js';
         $targetDir = $this->_initMergerDir('js');
         if (!$targetDir) {
             return '';
         }
-        // COPY>
 
+        // <kk: - after copied collecting of real file-path is done and after parent processing, add last-modified timestamp as url parameter
         $filename = $targetDir . DS . $targetFilename;
         return $this->addModifiedParam(
             parent::getMergedJsUrl($files),
             $filename
         );
+        // kk>
     }
 
     /**
